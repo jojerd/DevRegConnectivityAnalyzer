9259265a9f6cb0b426b5c98dbb3864782b7b4dd3
jojerd <jojerd@microsoft.com>
2024-06-01 20:24:40 -0400

    Script clean-up and additional comments.

--- DevRegConnectAnalyzer.ps1
+++ DevRegConnectAnalyzer.ps1
@@ -156,6 +156,7 @@ Write-Log -String "Public Firewall Enabled: $($PublicFirewall.Enabled)" -Name $L
 Write-Log -String "Pirvate Firewall Enabled: $($PrivateFirewall.Enabled)" -Name $Logname -OutHost
 Write-Log -String "Domain Firewall Enabled: $($DomainFirewall.Enabled)" -Name $Logname -OutHost
 
+# Retrieve USGov endpoint DNS records.
 function Get-USGov {
     $USGovTenantUrl = "https://login.microsoftonline.us/getuserrealm.srf?json=1&login=$TenantDomain"
     $USGovHRD = Invoke-RestMethod -Method Get -Uri $USGovTenantUrl
@@ -167,6 +168,7 @@ function Get-USGov {
     foreach ($Endpoint in $GovEndpoints) {
 
         try {
+             # Log successful retrieval of USGov Endpoint IP's.
             $DNS = Resolve-DnsName -Name $Endpoint | Select-Object IPAddress | Where-Object { $null -ne $_.IPAddress }
             Write-Host ""
             Write-Host "$Endpoint resolved successfully" -ForegroundColor Green
@@ -176,6 +178,7 @@ function Get-USGov {
             $IPHashtable.Add($Endpoint, $DNS.IPAddress)        
         }
         catch {
+            # Log failures and stop script because if we cannot retrieve the DNS records no point to continuing.
             Write-Log -String "Unable to resolve DNS for $Endpoint" -Name $Logname -OutHost
             Write-Host "Unable to resolve hostname $Endpoint, check DNS to confirm it is working correctly" -ForegroundColor Red
             Write-Host "Unable to continue to analyze connectivity, please resolve DNS resolution issue" -ErrorAction Stop
@@ -184,6 +187,7 @@ function Get-USGov {
     Write-Log -String "Checking DNS resolution for US Government Windows Hello for Business endpoints" -Name $Logname -OutHost
     foreach ($WHGEndpoint in $GovWindowsHelloEndpoints) {
         try {
+             # Log successful retrieval of USGov Windows Hello for Business endpoints.
             $WHGDNS = Resolve-DnsName -Name $WHGEndpoint | Select-Object IPAddress | Where-Object { $null -ne $_.IPAddress }
             Write-Host ""
             Write-Host "$Endpoint resolved successfully" -ForegroundColor Green
@@ -194,28 +198,31 @@ function Get-USGov {
             Write-Host "$WHGEndpoint resolved successfully" -ForegroundColor Green             
         }
         catch {
+            # Log failures and silently continue as Windows Hello for Business may not always be used in a USGov tenant.
             Write-Log -String "Unable to resolve DNS for $WHGEndpoint" -Name $Logname -OutHost
             Write-Host ""
             Write-Host "Unable to resolve hostname $WHGEndpoint, check DNS to confirm it is working correctly" -ForegroundColor Red
-            Write-Host "Unable to continue to analyze connectivity, please resolve DNS resolution issue" -ErrorAction Stop
+            Write-Host "Unable to continue to analyze connectivity, please resolve DNS resolution issue" -ErrorAction SilentlyContinue
         }
 
     }
+    # Write each endpoint and each IP Address resolved for that endpoint to the log.
     foreach ($HashEndpoint in $IPHashtable.Keys) {
         Write-Log -String "Endpoint: $HashEndpoint" -Name $Logname -OutHost
         foreach ($IPaddress in $IPHashtable[$HashEndpoint]) {
             Write-Log -String "IP: $IPaddress" -Name $Logname -OutHost
         }
     }
+    # Write to log that the function completed and inform which function is next.
     Write-Log -String "Completed function Get-USGov, calling next fuction to test Connectivity" -Name $Logname; Test-Connectivity -OutHost
 }
     
-
-
+# Function to retrieve Commercial endpoint DNS records.
 function Get-Commercial {
     foreach ($Endpoint in $CommercialEndpoints) {
 
         try {
+             # Log successful retrieval of Commercial Endpoint IP's.
             $DNS = Resolve-DnsName -Name $Endpoint | Select-Object IPAddress | Where-Object { $null -ne $_.IPAddress }
             Write-Host ""
             Write-Host "$Endpoint resolved successfully" -ForegroundColor Green 
@@ -225,6 +232,7 @@ function Get-Commercial {
                 
         }
         catch {
+            # Log failures and stop script because if we cannot retrieve the DNS records no point to continuing.
             Write-Host ""
             Write-Log -String "Unable to resolve DNS for $Endpoint" -Name $Logname -OutHost
             Write-Host "Unable to resolve hostname $Endpoint, check DNS to confirm it is working correctly" -ForegroundColor Red
@@ -243,19 +251,22 @@ function Get-Commercial {
             $IPHashtable.Add($WHCEndpoint, $WHDNS.IPAddress)   
         }
         catch {
+             # Log failures and silently continue as Windows Hello for Business may not always be used in a Commercial tenant.
             Write-Host ""
             Write-Log -String "Unable to resolve DNS for $WHCEndpoint" -Name $Logname -OutHost
             Write-Host "Unable to resolve hostname $WHCEndpoint, check DNS to confirm it is working correctly" -ForegroundColor Red
-            Write-Host "Unable to continue to analyze connectivity, please resolve DNS resolution issue" -ErrorAction Stop
+            Write-Host "Unable to continue to analyze connectivity, please resolve DNS resolution issue" -ErrorAction SilentlyContinue
         }
 
     }
+    # Write each endpoint and each IP Address resolved for that endpoint to the log.
     foreach ($HashEndpoint in $IPHashtable.Keys) {
         Write-Log -String "Endpoint: $HashEndpoint" -Name $Logname -OutHost
         foreach ($IPaddress in $IPHashtable[$HashEndpoint]) {
             Write-Log -String "IP: $IPaddress" -Name $Logname -OutHost
         }
     }
+    # Write to log that the function completed and inform which function is next.
     Write-Log -String "Completed function Get-Commercial, calling next fuction to test Connectivity" -Name $Logname; Test-Connectivity
 }
 
@@ -265,7 +276,7 @@ function Get-TenantInfo {
      
 
     Write-Log -String "Retrieving Tenant Information..." -Name $Logname -OutHost
-    #Checking the openid-configuration to pull information from.
+    # Checking the openid-configuration to pull information from.
     $url = "https://login.windows.net/$TenantDomain/.well-known/openid-configuration"
     $TenantInfo = Invoke-RestMethod -Method Get -Uri $url
     $TenantIDInfo = ($TenantInfo.authorization_endpoint).split("/")
@@ -276,7 +287,7 @@ function Get-TenantInfo {
     Write-Log -String "Cloud Instance: $CloudInstance" -Name $Logname -OutHost
     Write-Log -String "Tenant Cloud: $TenantCloud" -Name $Logname -OutHost
     #===========================================================================================
-    #Checking the get user realm endpoint to get home realm related information. 
+    # Checking the get user realm endpoint to get home realm related information. 
     $HRDUrl = "https://login.microsoftonline.com/getuserrealm.srf?json=1&login=$TenantDomain"
     $HomeRealmDiscoveryInfo = Invoke-RestMethod -Method Get -Uri $HRDUrl
     $DomainAuth = $HomeRealmDiscoveryInfo.NameSpaceType
@@ -285,7 +296,7 @@ function Get-TenantInfo {
     Write-Log -String "Tenant IDP URL: $IDP" -Name $Logname -OutHost
     #===========================================================================================
      
-     
+    # Switch to determine which function to call after tenant information has been retrieved.
     Switch ($TenantCloud) {
         "USG" { Write-Log -String "Tenant is in (USG) FairFax, calling Get-USGov function" -Name $Logname -OutHost; Get-USGov }
         "USGov" { Write-Log -String "Tenant is in (USGov) Arlington, calling Get-USGov function" -Name $Logname -OutHost; Get-USGov } 
@@ -296,41 +307,41 @@ function Get-TenantInfo {
 }
 
 function Test-Connectivity {
-    # Specify protocols we are going to use to test with to confirm TLS is not being interfered with.
+    # Specify protocols we are going to use to test with to confirm TLS is not being interfered with (Only testing with TLS 1.2 and TLS 1.3).
     $Protocols = [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Tls13
     $ProtocolArray = $Protocols -split ","
     [int]$Port = 443
-    #Specify Timeout to wait for connection in milliseconds.
+    # Specify Timeout to wait for connection in milliseconds.
     $Timeout = 500
-    #itinerate through the Hashtable for each endpoint IP address to check if Port is open.
+    # Itinerate through the Hashtable for each endpoint IP address to check if Port is open or closed.
     foreach ($HashEndpoint in $IPHashtable.Keys) {
         Write-Log -String "Checking connectivity for Endpoint: $HashEndpoint" -Name $Logname -OutHost
         foreach ($IPaddress in $IPHashtable[$HashEndpoint]) {
             Write-Log -String " " -Name $Logname -OutHost
             Write-Log -String "Checking IP Address: $IPaddress" -Name $Logname -OutHost
-            #Build the TCP Client to make the connection to IP and Port.
+            # Build the TCP Client to make the connection to IP and Port.
             $TCPClient = New-Object System.Net.Sockets.TcpClient
             $Portcheck = $TCPClient.ConnectAsync($IPaddress, $Port).Wait($Timeout)
-            #If connection is successful create a new variable with the port status and log it.
+            # If connection is successful create a new variable with the port status and log it.
             if ($Portcheck -eq 'True') {
                 $PortStatus = "Open"
                 $TcpClient.Close()
                 Write-Log -String "Port 443 for $HashEndpoint IP Address $IPaddress is $PortStatus" -Name $Logname -OutHost
             }
             else {
-                #If connection failed log the endpoint name and IP Address.
+                # If connection failed log the endpoint name and IP Address.
                 $PortStatus = "Closed"
                 Write-Log -String "Connection Timeout for $HashEndpoint IP Address: $IPaddress" -Name $Logname -OutHost
                 $TCPClient.Close()
             }
-            #If Port 443 is open test connectivity and obtain connection details.
+            # If Port 443 is open test connectivity and obtain connection details.
             if ($PortStatus -eq 'Open') {
                 foreach ($Protocol in $ProtocolArray) {
-                    #Build a new client to check TLS connectivity.
+                    # Build a new client to check TLS connectivity.
                     $SocketClient = New-Object System.Net.Sockets.Socket([System.Net.Sockets.SocketType]::Stream, [System.Net.Sockets.ProtocolType]::Tcp)
                     $SocketClient.Connect($IPAddress, $Port)
                     try {
-                        #Retrieve connection details to be able to log them (Remote Certificate, Cipher used, Protocol connected etc.)
+                        # Retrieve connection details to be able to log them (Remote Certificate, Cipher used, Protocol connected etc.)
                         $Stream = New-Object System.Net.Sockets.NetworkStream($SocketClient, $true)
                         $SecureChannel = New-Object System.Net.Security.SslStream($Stream, $true)
                         $SecureChannel.AuthenticateAsClient($HashEndpoint, $null, $Protocol, $false)
@@ -340,7 +351,7 @@ function Test-Connectivity {
                         Write-Host ""
                         Write-Host "SUCCESS!" -ForegroundColor Green
                         Write-Host "Connected to $HashEndpoint with IP: $IPaddress using TLS Version $Protocol" -ForegroundColor Green
-                        #Save connection details to a PS Custom Object to be written to the log file.
+                        # Save connection details to a PS Custom Object to be written to the log file.
                         $ConnectedReport = [PSCustomObject]@{
                             Hostname             = $HashEndpoint
                             IPAddress            = $IPAddress
@@ -357,7 +368,7 @@ function Test-Connectivity {
                             ProtocolSupported    = $ProtocolStatus
                         
                         }
-                        #Itinerate through the PS Custom Object and write details retrieved from TLS Connection above to log file.
+                        # Itinerate through the PS Custom Object and write details retrieved from TLS Connection above to log file.
                         foreach ($object in $ConnectedReport) {
                             Write-Log -String "+++++++++++++++++++++++++++++++++++ CERTIFICATE DETAILS +++++++++++++++++++++++++++++++++++++++++++++" -Name $Logname -OutHost
                             foreach ($i in $object.PSObject.Properties) {
@@ -369,7 +380,7 @@ function Test-Connectivity {
                     }
         
                     catch {
-                        #If Connection fails write to the to the log the hostname, IP address, Port and protocol used.
+                        # If Connection fails write to the log the hostname, IP address, Port and protocol used.
                         $ProtocolStatus = "False"
                         Write-Host ""
                         Write-Host "FAILURE!" -ForegroundColor Red
@@ -382,7 +393,7 @@ function Test-Connectivity {
                             ProtocolVersion   = $Protocol
                             ProtocolSupported = $ProtocolStatus
                         }
-                        #Itinerate through the PS Custom Object and write details retrieved from TLS connection failure details above to log file.
+                        # Itinerate through the PS Custom Object and write details retrieved from TLS connection failure details above to log file.
                         foreach ($object in $UnableConnectReport) {
                             Write-Log -String "+++++++++++++++++++++++++++++++++++ TLS FAILURE DETAILS +++++++++++++++++++++++++++++++++++++++++++++" -Name $Logname -OutHost
                             foreach ($i in $object.PSObject.Properties) {
@@ -391,13 +402,14 @@ function Test-Connectivity {
                             Write-Log -String "+++++++++++++++++++++++++++++++++++ TLS FAILURE DETAILS +++++++++++++++++++++++++++++++++++++++++++++" -Name $Logname -OutHost
                         }
                     }
-                    #Close the current connection to create a new one.
+                    # Close the current connection to create a new one.
                     $SocketClient.Dispose()
                     $SecureChannel.Dispose()
                 }
 
             }
             else {
+                # If port is closed log it and detail which endpoint and IP Address script was unable to connect to.
                 Write-Log -String "Skipping diagnostics for $IPAddress as destination port is closed or unreachable" -Name $Logname -OutHost
                 Write-Host ""
                 Write-Host "Unable to connect to $IPaddress for $HashEndpoint over port 443 as it is either being blocked by a firewall / proxy or is unreachable" -ForegroundColor Red
@@ -406,6 +418,7 @@ function Test-Connectivity {
             }
         }
     }
+    # Notify that script has completed.
     Write-Log -String "Script completed testing connectivity" -Name $Logname
     Write-Host ""
     Write-Host "Script Completed and details logged" -ForegroundColor Yellow
@@ -413,7 +426,3 @@ function Test-Connectivity {
    
 }
 Get-TenantInfo
-     
-
-
-
